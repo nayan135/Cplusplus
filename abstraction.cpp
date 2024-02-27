c5dd7b87c3113b2235064d76b917ec41454ac97e
NAYAN <acharyanayan604@gmail.com>
2024-02-27 22:23:29 +0545

    Add abstraction.cpp file with Abstraction class implementation

---
+++ abstraction.cpp
@@ -0,0 +1,30 @@
+// C++ Program to Demonstrate the
+// working of Abstraction
+#include <iostream>
+using namespace std;
+
+class Abstraction {
+private:
+	int a, b;
+
+public:
+	void set(int x, int y)
+	{
+		a = x;
+		b = y;
+	}
+
+	void display()
+	{
+		cout << "a = " << a << endl;
+		cout << "b = " << b << endl;
+	}
+};
+
+int main()
+{
+	implementAbstraction obj;
+	obj.set(10, 20);
+	obj.display();
+	return 0;
+}
