336efffe7cce39b10606f91b478637523054b428
NAYAN <acharyanayan604@gmail.com>
2024-02-27 16:49:26 +0545

    Fix ambiguity in multiple inheritance

--- ambiguity in multiple inheritance.cpp
+++ ambiguity in multiple inheritance.cpp
@@ -3,24 +3,25 @@ using namespace std;
 class base1{
     public:
     void Ambiguity(){
-        cout<<"base 1";
+        cout<<"base 1"<<endl;
     }
 };
 class base2{
     public:
     void Ambiguity(){
-        cout<<"base 2";
+        cout<<"base 2"<<endl;
     }
 };
 class derived:public base1, public base2{
     public:
     void Ambiguity(){
-        cout<<"base 3";
+        cout<<"base 3"<<endl;
     }
 };
 int main(){
     derived obj;
    obj.base1::Ambiguity();
    obj.base2::Ambiguity();
+   obj.Ambiguity();
     return 0;
 }
