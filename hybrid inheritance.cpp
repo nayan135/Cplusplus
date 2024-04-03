2dfaf8e27b4903d9f5daed57c7702f088ec955c1
NAYAN <acharyanayan604@gmail.com>
2024-04-03 17:08:58 +0545

    Fix function names and add newline characters

--- hybrid inheritance.cpp
+++ hybrid inheritance.cpp
@@ -3,32 +3,32 @@ using namespace std;
 class hybrid{
 public:
 void base(){
-    cout<<"base class";
+    cout<<"base class\n";
 }
 };
 class derived:public hybrid{
 public:
 void derive(){
-    cout<<"derived class";
+    cout<<"derived class\n";
 }
 };
 class Child:public derived{
 public:
 void child(){
-    cout<<"child class";
+    cout<<"child class\n";
 }
 };
 class child1:public derived{
 public:
 void child2(){
-    cout<<"child1 class";   
+    cout<<"child1 class\n";   
 }
 };
 int main(){
   child1 c;
   Child c1;
   c.base();
-  c.derived();
+  c.derive();
   c1.child();
   c.child2();
     return 0;
